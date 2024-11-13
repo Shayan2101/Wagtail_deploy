12241a28edeb0a81471b809a6c16f003e2ed6d50
Shayan2101 <shayanahmadi2101@gmail.com>
2024-11-13 07:39:03 +0330

    Initial commit

---
+++ linux_mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-#6-qa$6($zk(8d&^z4q)#9ee(+zdr7x92rj&eume_0ojuv!6p9"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
